699af354eccce2916055f682fb471bdee4ac0852
hieunugent <hieunugent@gmail.com>
2020-11-09 00:47:55 -0800

    new

--- App.js
+++ App.js
@@ -11,32 +11,59 @@ import {
 import Todo from './Todo';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [todos, setTodo] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
 
-  const addTodo = () => {
-    setTodo([input, ...todos]);
-    setInput('');
+  const [gigs, setGigs] = useState([
+    {
+      description: 'Freelance job with Me',
+      amount: 409.99,
+    },
+  ]);
+
+  const addGig = () => {
+    setGigs([
+      ...gigs,
+      {
+        description: description,
+        amount: amount,
+      },
+    ]);
+    setDescription('');
+    setAmount('');
   };
   return (
     <SafeAreaView>
       <View>
         <Text style={styles.titleText}>My Todo List App </Text>
       </View>
-      <ScrollView>
-        {todos.map((todo) => (
-          <Todo item={todo} />
-        ))}
-      </ScrollView>
 
       <TextInput
-        value={input}
-        onChangeText={(text) => setInput(text)}
-        placeholder="enter some thing"
+        value={description}
+        onChangeText={(text) => setDescription(text)}
+        placeholder="what did you do"
         style={styles.inputText}
       />
-
-      <Button onPress={addTodo} title="Add TODO List" />
+      <TextInput
+        value={amount}
+        onChangeText={(amount) => setAmount(amount)}
+        placeholder="how much you make"
+        keyboardType="numeric"
+        style={styles.inputText}
+      />
+      <Button
+        disabled={!amount && !description}
+        onPress={addGig}
+        title="Add Gig"
+      />
+      
+         {gigs.map((index , gig) => (
+          <View  key={index}>
+          <Text>{gig.description}</Text>
+          <Text>{gig.amount}</Text>
+          </View>
+         ))}
+      
     </SafeAreaView>
   );
 };
