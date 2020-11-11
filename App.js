c061b82581e51a1f21b6659c7f5887b2efdbac02
hieunugent <hieunugent@gmail.com>
2020-11-10 23:52:05 -0800

    add profile

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
@@ -7,26 +7,58 @@ import {
   TextInput,
   Button,
   ScrollView,
+  Dimensions,
 } from 'react-native';
-import Todo from './Todo';
+import { LineChart, BarChart } from 'react-native-chart-kit'
 
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
+  const [total, setTotal] = useState(0);
+  const [data, setData] = useState([
+    {[new Date()]: 2000}, 
+    {[new Date() - 1] : 2500},
+  ])
 
   const [gigs, setGigs] = useState([
     {
       description: 'Freelance job with Me',
+      timestamp: new Date(),
       amount: 409.99,
     },
   ]);
+  useEffect(() => {
+    setTotal(gigs.reduce((total, gig) => {
+      return total + Number(gig.amount)
+    }, 0))
+  }, [gigs])
 
+
+  // const chartConfig = {
+  //   backgroundGradientFrom: "green",
+  //   backgroundGradientFromOpacity: 0,
+  //   backgroundGradientTo: "green",
+  //   backgroundGradientToOpacity: 0.5,
+  //   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  //   strokeWidth: 2, // optional, default 3
+  //   barPercentage: 0.5,
+  //   useShadowColorFromDataset: false // optional
+  // };
+  // const data = {
+  //   labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
+  //   datasets: [
+  //     {
+  //       data: {}
+  //     }
+  //   ]
+  // };
   const addGig = () => {
     setGigs([
       ...gigs,
       {
         description: description,
         amount: amount,
+        timestamp: new Date(),
       },
     ]);
     setDescription('');
@@ -34,9 +66,56 @@ const App = () => {
   };
   return (
     <SafeAreaView>
+
+
       <View>
         <Text style={styles.titleText}>My Todo List App </Text>
       </View>
+
+
+      <View>
+
+        {/* <BarChart
+          // style={graphStyle}
+          data={data}
+          
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        /> */}
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={data}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "steelblue",
+            backgroundGradientFrom: "steelblue",
+            backgroundGradientTo: "steelblue",
+            decimalPlaces: null, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "blue"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
+      <Text>Total Income: $ {total}</Text>
 
       <TextInput
         value={description}
@@ -56,13 +135,13 @@ const App = () => {
         onPress={addGig}
         title="Add Gig"
       />
-      
-         {gigs.map((index , gig) => (
-          <View  key={index}>
+
+      {gigs.map((gig) => (
+        <View  >
           <Text>{gig.description}</Text>
-          <Text>{gig.amount}</Text>
-          </View>
-         ))}
+          <Text> $ {gig.amount}</Text>
+        </View>
+      ))}
       
     </SafeAreaView>
   );
