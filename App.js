502e5d8a7d71646112d9d2c82cbe2fffeb4ba9ac
hieunugent <hieunugent@gmail.com>
2020-11-13 01:31:46 -0800

    format and edit amount and date

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React, { Component, useEffect, useState } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {
   StyleSheet,
   SafeAreaView,
@@ -10,17 +10,47 @@ import {
   ScrollView,
   Dimensions,
 } from 'react-native';
-import { LineChart, BarChart } from 'react-native-chart-kit'
+import {LineChart, BarChart} from 'react-native-chart-kit';
 
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
   const [total, setTotal] = useState(0);
   const [data, setData] = useState([
-    {[moment()]: 2000}, 
-    {[moment().subtract(1, 'day')] : 2500},
-  ])
+    {date: moment().format('LL'), amount: 2000},
+    {date: moment().subtract(1, 'day').format('LL'), amount: 2500},
+    {date: moment().subtract(2, 'day').format('LL'), amount: 1500},
+    {date: moment().subtract(2, 'day').format('LL'), amount: 2500},
+    {date: moment().subtract(2, 'day').format('LL'), amount: 3500},
+    {date: moment().subtract(3, 'day').format('LL'), amount: 4500},
+    {date: moment().subtract(4, 'day').format('LL'), amount: 5500},
+  ]);
+  const getDates = () => data.map((pair) => pair.date);
+  const getAmounts = () => data.map((pair) => pair.amount);
+  // [data1, data2, data3, ,data4, data5]
+  // ['10/27/2020'] -> '10/27/2020'
+  const transformData = (groupedData) => {
+    const transformedArray = [];
+    Object.entries(groupedData).forEach((entry) => {
+      const total = entry[1].reduce((total, pair) => total + pair.amount, 0);
+      transformedArray.push({date: entry[0], amount: total});
+    });
 
+    return transformedArray;
+  };
+
+  const groupBy = (array, key) =>
+    array.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+
+  console.log('Debug', data);
+  console.log('The date', getDates());
+  console.log('the amount', getAmounts());
+
+  console.log('the group by ', Object.entries(groupBy(data, 'date')));
+  console.log('total group by value', transformData(groupBy(data, 'date')));
   const [gigs, setGigs] = useState([
     {
       description: 'Freelance job with Me',
@@ -29,11 +59,12 @@ const App = () => {
     },
   ]);
   useEffect(() => {
-    setTotal(gigs.reduce((total, gig) => {
-      return total + Number(gig.amount)
-    }, 0))
-  }, [gigs])
-
+    setTotal(
+      gigs.reduce((total, gig) => {
+        return total + Number(gig.amount);
+      }, 0),
+    );
+  }, [gigs]);
 
   // const chartConfig = {
   //   backgroundGradientFrom: "green",
@@ -45,14 +76,14 @@ const App = () => {
   //   barPercentage: 0.5,
   //   useShadowColorFromDataset: false // optional
   // };
-  // const data = {
-  //   labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
-  //   datasets: [
-  //     {
-  //       data: [10,23,11,98,90]
-  //     }
-  //   ]
-  // };
+  const dada = {
+    labels: getDates(),
+    datasets: [
+      {
+        data: getAmounts(),
+      },
+    ],
+  };
   const addGig = () => {
     setGigs([
       ...gigs,
@@ -67,15 +98,10 @@ const App = () => {
   };
   return (
     <SafeAreaView>
-
-
       <View>
         <Text style={styles.titleText}>My Todo List App </Text>
       </View>
-
-
       <View>
-
         {/* <BarChart
           // style={graphStyle}
           data={data}
@@ -87,32 +113,39 @@ const App = () => {
         /> */}
         <Text>Bezier Line Chart</Text>
         <LineChart
-          data={data}
-          width={Dimensions.get("window").width} // from react-native
+          data={{
+            labels: getDates(),
+            datasets: [
+              {
+                data: getAmounts(),
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
           height={220}
           yAxisLabel="$"
           yAxisSuffix="k"
           yAxisInterval={1} // optional, defaults to 1
           chartConfig={{
-            backgroundColor: "steelblue",
-            backgroundGradientFrom: "steelblue",
-            backgroundGradientTo: "steelblue",
+            backgroundColor: 'steelblue',
+            backgroundGradientFrom: 'steelblue',
+            backgroundGradientTo: 'steelblue',
             decimalPlaces: null, // optional, defaults to 2dp
             color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
             labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
             style: {
-              borderRadius: 16
+              borderRadius: 16,
             },
             propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "blue"
-            }
+              r: '6',
+              strokeWidth: '2',
+              stroke: 'blue',
+            },
           }}
           bezier
           style={{
             marginVertical: 8,
-            borderRadius: 16
+            borderRadius: 16,
           }}
         />
       </View>
@@ -138,12 +171,11 @@ const App = () => {
       />
 
       {gigs.map((gig) => (
-        <View  >
+        <View>
           <Text>{gig.description}</Text>
           <Text> $ {gig.amount}</Text>
         </View>
       ))}
-      
     </SafeAreaView>
   );
 };
