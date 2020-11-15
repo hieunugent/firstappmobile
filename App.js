9fce7e2f5917ad52b4de73907ad786a328a3d3bf
hieunugent <hieunugent@gmail.com>
2020-11-14 20:41:29 -0800

    fix

--- App.js
+++ App.js
@@ -16,9 +16,19 @@ const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
   const [total, setTotal] = useState(0);
+  const [transformedData, setTransformedData] = useState([]);
+
+
+  const groupBy = (array, key) =>
+    array.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+
 
 
   const [data, setData] = useState([
+    
     {date: moment().format('LL'), amount: 2000},
     {date: moment().subtract(1, 'day').format('LL'), amount: 1000},
     {date: moment().subtract(1, 'day').format('LL'), amount: 1000},
@@ -32,29 +42,27 @@ const App = () => {
     {date: moment().subtract(2, 'day').format('LL'), amount: 400},
     {date: moment().subtract(2, 'day').format('LL'), amount: 500},
   ]);
-  const [transformedData, setTransformedData] = useState([]);
-  useEffect(() => {
-    setTransformedData(transformData(groupBy(data, 'date')));
-  }, [data]);
- 
   const transformData = (groupedData) => {
     const transformedArray = [];
+  
     Object.entries(groupedData).forEach((entry) => {
       const total = entry[1].reduce((total, pair) => total + pair.amount, 0);
-      transformedArray.push({date: moment(entry[0]).format('MM/DD'), amount: total});
+      transformedArray.push({ date: moment(entry[0]).format('MM/DD'), amount: total });
     });
-    const sortedArray = transformedArray.sort((a, b) => moment(a['date']).diff(moment(b['date'])))
+    const sortedArray = transformedArray.sort((a, b) => (moment(a['date'])).diff(moment(b['date'])))
     return sortedArray;
   };
   const getDates = () => transformedData.map((pair) => pair.date);
   const getAmounts = () => transformedData.map((pair) => pair.amount);
+  useEffect(() => {
+    setTransformedData(transformData(groupBy(data, 'date')));
+  }, [data]);
+ 
+  
+ 
   // [data1, data2, data3, ,data4, data5]
   // ['10/27/2020'] -> '10/27/2020'
-  const groupBy = (array, key) =>
-    array.reduce((rv, x) => {
-      (rv[x[key]] = rv[x[key]] || []).push(x);
-      return rv;
-    }, {});
+  
 
   console.log('Debug', data);
   console.log('The date', getDates());
@@ -62,6 +70,7 @@ const App = () => {
 
   console.log('the group by ', Object.entries(groupBy(data, 'date')));
   console.log('total group by value', transformData(groupBy(data, 'date')));
+  transformedData.map(pair => console.log(pair.date , ":", pair.amount));
   const [gigs, setGigs] = useState([
     {
       description: 'Freelance job with Me',
@@ -77,24 +86,13 @@ const App = () => {
     );
   }, [gigs]);
 
-  // // const chartConfig = {
-  // //   backgroundGradientFrom: "green",
-  // //   backgroundGradientFromOpacity: 0,
-  // //   backgroundGradientTo: "green",
-  // //   backgroundGradientToOpacity: 0.5,
-  // //   color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  // //   strokeWidth: 2, // optional, default 3
-  // //   barPercentage: 0.5,
-  // //   useShadowColorFromDataset: false // optional
-  // // };
-
+  
   const addGig = () => {
     setGigs([
       ...gigs,
       {
         description: description,
         amount: Number(amount),
-        
       },
     ]);
     setData([
@@ -108,7 +106,7 @@ const App = () => {
   return (
     <SafeAreaView>
       <View>
-        <Text style={styles.titleText}>  MyAPP </Text>
+        <Text style={styles.titleText}> app v1.1</Text>
       </View>
       <View>
         <Text>Bezier Line Chart</Text>
